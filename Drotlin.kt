837e0a5f4e986256e8e734ca60e30232791afda1
Xuebin Shang <shangxuebin1991@gmail.com>
2018-06-07 10:05:09 +0800

    去掉资源相关，请用anko

--- Drotlin.kt
+++ Drotlin.kt
@@ -1,263 +1,301 @@
-package drotlin
+@file:Suppress("DEPRECATION", "UNCHECKED_CAST")
 
+package androidy
+
+import android.annotation.SuppressLint
 import android.app.Activity
-import android.app.Fragment
-import android.arch.lifecycle.ViewModel
-import android.arch.lifecycle.ViewModelProviders
-import android.support.v4.app.Fragment as FragmentV4
+import android.app.ActivityManager
+import android.app.Application
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.Drawable
 import android.net.Uri
-import android.os.Build
-import android.os.Environment
-import android.os.Looper
+import android.os.Build.VERSION.SDK_INT
 import android.os.Handler
-import android.support.v4.app.FragmentActivity
+import android.os.Looper
+import android.support.annotation.ColorRes
+import android.support.annotation.DrawableRes
+import android.support.annotation.IntegerRes
+import android.support.annotation.StringRes
+import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
-import android.support.v7.app.AlertDialog
-import android.util.TypedValue
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
+import android.support.v4.content.res.ResourcesCompat
+import android.support.v4.view.ViewCompat
+import android.util.AndroidRuntimeException
+import android.view.*
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.TextView
 import java.io.File
-import java.lang.StringBuilder
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.reflect.KClass
 
-typealias GroupLayoutParams = android.view.ViewGroup.LayoutParams
-typealias MarginLayoutParams = android.view.ViewGroup.MarginLayoutParams
-typealias LinearLayoutParams = android.widget.LinearLayout.LayoutParams
-typealias FrameLayoutParams = android.widget.FrameLayout.LayoutParams
-typealias Permissions = android.Manifest.permission
 
-fun initDrotlin(context: Context) {
-    drotlin.applicationContext = context.applicationContext
-}
-
-//Context==================================================
-lateinit var applicationContext: Context
-
-fun Activity.alert(message: String) {
-    AlertDialog.Builder(this)
-            .setTitle("提示")
-            .setMessage(message)
-            .setPositiveButton("ok", { d, _ -> d.dismiss() })
-            .show()
-}
-
-fun Fragment.alert(message: String) {
-    activity?.alert(message)
-}
-
-fun FragmentV4.alert(message: String) {
-    activity?.alert(message)
-}
-
-inline fun <T : Activity> Context.startActivity(clazz: KClass<T>, handler: Intent.() -> Unit = {}) {
-    val intent = Intent(this, clazz.java)
-    handler(intent)
-    if (this !is Activity) {
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+@SuppressLint("StaticFieldLeak")
+lateinit var xContext: Context
+var xDebugMode = false
+inline fun Application.initY(init: () -> Unit = {}) {
+    xContext = this
+    xDebugMode = (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0
+    if (currentProcessName == packageName) {
+        init()
     }
-    startActivity(intent)
 }
 
-inline fun <reified T : ViewModel> FragmentActivity.getViewModel(): T {
-    return ViewModelProviders.of(this).get(T::class.java)
+val currentProcessName: String?
+    get() {
+        val pid = android.os.Process.myPid()
+        val activityManager = xContext
+                .getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        activityManager.runningAppProcesses.forEach {
+            if (it.pid == pid) {
+                return it.processName
+            }
+        }
+        return null
+    }
+
+//Test====================================================================================================
+fun Exception.throwWhenDebug() {
+    if (xDebugMode) {
+        throw AndroidRuntimeException(this)
+    }
+}
+
+inline fun <T> T.applyWhenDebug(block: T.() -> Unit): T {
+    if (xDebugMode) {
+        block()
+    }
+    return this
+}
+
+val Int.valueWhenDebug: Int
+    get() = if (xDebugMode) {
+        this
+    } else 0
+val String.valueWhenDebug: String?
+    get() = if (xDebugMode) {
+        this
+    } else null
+
+
+fun xLog(tag: Any?, msg: Any?) {
+    if (xDebugMode) {
+        val tagS = (tag as? String) ?: if (tag != null) {
+            tag::class.simpleName
+        } else {
+            null
+        } ?: "TAG"
+        val msgS = """
+            |Log->${"yyyy-MM-dd HH:mm:ss.SSS".formatDate(System.currentTimeMillis())} [${xContext.packageName}]
+            |TAG->$tagS
+            |MSG->${when (msg) {
+            is CharSequence -> msg
+            is Throwable -> "${msg.message}\n${msg.stackTrace.joinToString("\n    ->")}"
+            is Any -> msg::class.simpleName
+            else -> null
+        } ?: "NNN null object NNN"}
+            """.trimMargin()
+        android.util.Log.d(tagS, msgS)
+        //System.out.println(msgS)
+    }
 }
 
 
-inline fun <reified T : ViewModel> FragmentV4.getViewModel(): T {
-    return ViewModelProviders.of(this).get(T::class.java)
+//UI====================================================================================================
+
+
+fun Context.color(@ColorRes id: Int): Int = ResourcesCompat.getColor(resources, id, null)
+fun Context.drawable(@DrawableRes id: Int): Drawable? = ResourcesCompat.getDrawable(resources, id, null)
+fun Context.integer(@IntegerRes id: Int): Int = resources.getInteger(id)
+fun View.color(@ColorRes id: Int): Int = context.color(id)
+fun View.drawable(@DrawableRes id: Int): Drawable? = context.drawable(id)
+fun View.integer(@IntegerRes id: Int): Int = context.integer(id)
+fun View.string(@StringRes id: Int): String = context.getString(id)
+
+fun noGetter(): Nothing = throw AndroidRuntimeException("No getter")
+
+var TextView.hintTextColorResource: Int
+    get() = noGetter()
+    set(value) {
+        setHintTextColor(context.color(value))
+    }
+
+val Int.measureSpecExactly
+    get() = View.MeasureSpec.makeMeasureSpec(this, View.MeasureSpec.EXACTLY)
+
+var View.xBackground: Drawable?
+    get() = background
+    set(value) = ViewCompat.setBackground(this, value)
+
+fun ViewGroup.inflate(layoutId: Int, attach: Boolean = false): View {
+    return LayoutInflater.from(context).inflate(layoutId, this, attach)
 }
 
-fun toast(message: String) {
-    val toast = Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT)
-    toast.setGravity(Gravity.CENTER, 0, 0)
-    toast.show()
+fun Context.inflate(layoutId: Int, parent: ViewGroup? = null, attach: Boolean = false): View {
+    return LayoutInflater.from(this).inflate(layoutId, parent, attach)
+}
+
+fun Bitmap.save2File(file: File): File? {
+    try {
+        val fOut = FileOutputStream(file)
+        compress(Bitmap.CompressFormat.PNG, 100, fOut)
+        fOut.flush()
+        fOut.close()
+        return file
+    } catch (e: Exception) {
+        e.throwWhenDebug()
+    }
+    return null
+}
+
+fun TextView.enableUnderline() {
+    paint.flags = paint.flags or Paint.UNDERLINE_TEXT_FLAG
+}
+
+fun Activity.enableImmersionMode() {
+    if (SDK_INT >= 19) {
+        if (SDK_INT >= 21) {
+            window.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
+        } else {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        }
+    }
+}
+
+val View.statusBarHeight: Int
+    get() {
+        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            //根据资源ID获取响应的尺寸值
+            try {
+                return resources.getDimensionPixelSize(resourceId)
+            } catch (e: Exception) {
+                e.throwWhenDebug()
+            }
+        }
+        return 0
+    }
+
+inline fun <T : View> T.flparams(
+        width: Int = android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
+        height: Int = android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
+        gravity: Int = Gravity.NO_GRAVITY,
+        init: FrameLayout.LayoutParams.() -> Unit = {}
+): T {
+    val layoutParams = FrameLayout.LayoutParams(width, height, gravity)
+    layoutParams.init()
+    this@flparams.layoutParams = layoutParams
+    return this
+}
+
+inline fun <T : View> T.llparams(
+        width: Int = android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
+        height: Int = android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
+        gravity: Int = Gravity.NO_GRAVITY,
+        weight: Float = 0f,
+        init: LinearLayout.LayoutParams.() -> Unit = {}
+): T {
+    val layoutParams = LinearLayout.LayoutParams(width, height, weight)
+    layoutParams.gravity = gravity
+    layoutParams.init()
+    this@llparams.layoutParams = layoutParams
+    return this
 }
 
 
-//Thread==================================================
-val mainHandler = Handler(Looper.getMainLooper())
+//Thread====================================================================================================
+val mainHandler by lazy { Handler(Looper.getMainLooper()) }
+val isMainThread: Boolean
+    get() = Thread.currentThread() === Looper.getMainLooper().thread
 
-fun <T> T.runOnUiThread(delay: Long = 0, runnable: T.() -> Unit) {
-    mainHandler.postDelayed({ runnable() }, delay)
+fun xRunOnUiThread(delay: Long = 0, run: () -> Unit) {
+    if (0L == delay && isMainThread) {
+        run()
+    }
+    mainHandler.postDelayed(run, delay)
 }
 
-fun runOnUiThread(delay: Long = 0, runnable: () -> Unit) {
-    mainHandler.postDelayed({ runnable() }, delay)
-}
+//string====================================================================================================
+private val dateFormatLocal = ThreadLocal<SimpleDateFormat>()
 
-//Ui==================================================
-fun View.inflate(layoutId: Int, viewGroup: ViewGroup? = parent as? ViewGroup, attachToRoot: Boolean = false): View {
-    return LayoutInflater.from(context).inflate(layoutId, viewGroup, attachToRoot)
-}
-
-fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
-    return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
-}
-
-fun Context.inflate(layoutId: Int, viewGroup: ViewGroup? = null, attachToRoot: Boolean = false): View {
-    return LayoutInflater.from(this).inflate(layoutId, viewGroup, attachToRoot)
-}
-
-val Int.dimensionPixelSize: Int
-    get() = applicationContext.resources.getDimensionPixelSize(this)
-
-val Number.dip2Px: Int
-    get() = TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP, toFloat(), applicationContext.resources.displayMetrics)
-            .toInt()
-
-val Int.colorResource: Int
-    get() = if (Build.VERSION.SDK_INT >= 23) {
-        applicationContext.resources.getColor(this, applicationContext.theme)
+fun getDateFormat(pattern: String): SimpleDateFormat {
+    val df = dateFormatLocal.get()
+    return if (df != null) {
+        df.applyPattern(pattern)
+        df
     } else {
-        applicationContext.resources.getColor(this)
+        val df2 = SimpleDateFormat(pattern, Locale.CHINA)
+        dateFormatLocal.set(df2)
+        df2
     }
-
-val Int.stringResource: String
-    get() = applicationContext.resources.getString(this)
-
-fun Int.setAlpha(alpha: Int): Int {
-    return (this and 0x00ffffff) or (alpha shl 24)
 }
 
-fun Int.toMeasureSpec(mode: Int): Int {
-    return View.MeasureSpec.makeMeasureSpec(this, mode)
+fun String.parseDate(timeString: String): Date {
+    return getDateFormat(this).parse(timeString)
 }
 
-operator fun ViewGroup.plus(child: View): ViewGroup {
-    this.addView(child)
-    return this
+fun String.formatDate(date: Any): String {
+    return getDateFormat(this).format(date)
 }
 
-operator fun ViewGroup.minus(child: View): ViewGroup {
-    this.removeView(child)
-    return this
-}
-
-operator fun ViewGroup.get(index: Int): View? {
-    return getChildAt(index)
-}
-
-//File==================================================
-val File?.exists: Boolean
-    get() = this != null && exists()
-
-val File?.isDirectory: Boolean
-    get() = this != null && this.exists() && this.isDirectory
-
-val pictureDir: File
-    get() = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-
-fun String.toFile(): File {
-    return File(this)
-}
-
-val File.uri: Uri
-    get() = Uri.fromFile(this)
-
-//String==================================================
-val dateFormat by lazy { SimpleDateFormat("yyyy-MM-dd", Locale.CHINA) }
-
-val String.uri: Uri
-    get() = Uri.parse(this)
-
-fun Any?.stringValue(nullReplace: String = ""): String {
-    return when {
-        this == null -> nullReplace
-        this is String -> this
-        else -> return toString()
+fun String?.safeFloat(def: Float = 0f): Float {
+    try {
+        if (this != null) {
+            return toFloat()
+        }
+    } catch (e: Exception) {
+        e.throwWhenDebug()
     }
+    return def
+}
+
+operator fun StringBuilder.plus(str: String): StringBuilder {
+    return append(str)
+}
+
+
+//context====================================================================================================
+inline fun Context.xStartActivity(target: KClass<out Activity>, intent: Intent.() -> Unit = {}) {
+    startActivity(Intent(this, target.java).apply(intent))
+}
+
+inline fun View.xStartActivity(target: KClass<out Activity>, intent: Intent.() -> Unit = {}) {
+    context.xStartActivity(target, intent)
+}
+
+inline fun Fragment.xStartActivity(target: KClass<out Activity>, intent: Intent.() -> Unit = {}) {
+    startActivity(Intent(context ?: xContext, target.java).apply(intent))
+}
+
+fun <S> String.getSystemService(): S {
+    return xContext.getSystemService(this) as S
 }
 
 val String.PERMISSION_GRANTED: Boolean
-    get() = ContextCompat.checkSelfPermission(applicationContext, this) == PackageManager.PERMISSION_GRANTED
+    get() = ContextCompat.checkSelfPermission(xContext, this) == PackageManager.PERMISSION_GRANTED
 
-val String.PERMISSION_DENIED: Boolean
-    get() = ContextCompat.checkSelfPermission(applicationContext, this) == PackageManager.PERMISSION_DENIED
-
-fun String.toDate(pattern: String): Date? {
-    dateFormat.applyPattern(pattern)
-    return try {
-        dateFormat.parse(this)
-    } catch (e: Exception) {
-        null
-    }
-}
-
-fun Number.formatDate(pattern: String): String? {
-    dateFormat.applyPattern(pattern)
-    return try {
-        dateFormat.format(this)
-    } catch (e: Exception) {
-        null
-    }
-}
-
-fun StringBuilder.clear(): StringBuilder {
-    return delete(0, length)
-}
-
-operator fun StringBuilder.plus(s: String?): StringBuilder {
-    if(s!=null){
-        append(s)
-    }
-    return this
-}
-
-operator fun StringBuilder.minus(count: Int): StringBuilder {
-    if (count > length) {
-        delete(0, length)
-    } else {
-        delete(0, length - count)
-    }
-    return this
-}
-
-operator fun StringBuilder.times(time: Int): StringBuilder {
-    when{
-        time<=0 -> clear()
-        time==1 -> {}
-        time == 2 ->append(toString())
-        else -> {
-            val current = toString()
-            loopDo(time-1){append(current)}
+val Array<String>.PERMISSION_GRANTED: Boolean
+    get() {
+        if (Version.SDK_INT >= 23) {
+            forEach {
+                if (!it.PERMISSION_GRANTED) {
+                    return false
+                }
+            }
         }
+        return true
     }
-    return this
-}
-
-fun StringBuilder.nextLine(): StringBuilder {
-    append("\n")
-    return this
-}
-
-fun StringBuilder.tab(): StringBuilder{
-    space(4)
-    return this
-}
-
-fun StringBuilder.space(count: Int = 1): StringBuilder{
-    appendMultiTimes(count," ")
-    return this
-}
-
-fun StringBuilder.appendMultiTimes(time: Int, s: String): StringBuilder{
-    if(time==1){
-        append(s)
-    }else if(time>1){
-        loopDo(time){append(s)}
-    }
-    return this
-}
 
 //Other==================================================
 
@@ -276,8 +314,24 @@ inline fun <T> T.loopApply(count: Int, block: T.(Int) -> T): T {
     return t
 }
 
+val File.isDirectoryOrCreate: Boolean
+    get() {
+        if (!exists()) {
+            mkdirs()
+        }
+        return isDirectory
+    }
 
-fun test() {
-    var sb = StringBuilder()
-    sb += "5"
+fun File.toUri(): Uri {
+    return Uri.fromFile(this)
+}
+
+fun Uri.toFile(): File {
+    return File(path ?: toString())
+}
+
+inline fun Any?.applyWhenNull(run: () -> Unit) {
+    if (this == null) {
+        run()
+    }
 }
