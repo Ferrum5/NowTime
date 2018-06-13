ed9cd3cf7b24245fe8d4a98c7d143bafecdf58fa
Xuebin Shang <shangxuebin1991@gmail.com>
2018-06-13 09:47:40 +0800

    改为Alarm实现

--- app/src/main/java/net/alpacaplayground/nowtime/MainActivity.kt
+++ app/src/main/java/net/alpacaplayground/nowtime/MainActivity.kt
@@ -1,13 +1,17 @@
 package net.alpacaplayground.nowtime
 
+import android.Manifest
 import android.arch.lifecycle.Observer
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
 
     private lateinit var textView: TextView
 
@@ -15,15 +19,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<View>(R.id.buttonReport).setOnClickListener { startService(Intent(this, MainService::class.java)) }
-        findViewById<View>(R.id.buttonExit).setOnClickListener { stopService(Intent(this, MainService::class.java)) }
+        findViewById<View>(R.id.buttonExit).setOnClickListener {
+            stopService(Intent(this, MainService::class.java))
+            stopService(Intent(this, TimeNowService::class.java))
+            finish()
+        }
         val textMessage: TextView = findViewById(R.id.textView)
         liveMsg.observe(this, Observer {
             textMessage.text = it
         })
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SET_ALARM) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SET_ALARM), 1)
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        stopService(Intent(this,MainService::class.java))
+        stopService(Intent(this, MainService::class.java))
     }
 }
--- app/src/main/java/net/alpacaplayground/nowtime/TimeNowWorker.kt
+++ app/src/main/java/net/alpacaplayground/nowtime/TimeNowWorker.kt
@@ -1,20 +1,27 @@
 package net.alpacaplayground.nowtime
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.arch.lifecycle.MutableLiveData
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Build.VERSION.SDK_INT
 import android.os.Handler
 import android.os.HandlerThread
 import android.os.Message
 import android.speech.tts.TextToSpeech
+import android.support.v4.app.AlarmManagerCompat
 import android.util.Log
 import android.widget.TextView
 import java.text.SimpleDateFormat
 import java.util.*
 
+private const val WhatTimeNow = 1
+private const val WhatReleaseTts = 2
+
 class TimeNowWorker(val context: Context) : Handler.Callback {
-    private val calendar = Calendar.getInstance()
+    private var nextAlert = 0L
     private var tts: TextToSpeech? = null
     private var ttsPrepare = TextToSpeech.ERROR
 
@@ -28,72 +35,76 @@ class TimeNowWorker(val context: Context) : Handler.Callback {
 
     val liveMsg = MutableLiveData<String>()
 
-    fun timeNow() {
-        timerHandler.sendEmptyMessage(1)
-    }
-
     override fun handleMessage(msg: Message): Boolean {
-        if (msg.what == 2) { //释放tts
-            val tts = this.tts
-            if (tts != null) {
-                tts.shutdown()
-                this.tts = null
-                ttsPrepare = TextToSpeech.ERROR
-                Log.i("NowTime", "释放TTS")
-            }
-        } else if (msg.what == 1) {
-            timerHandler.removeMessages(1)//清除语音消息
-            timerHandler.removeMessages(2)//清除释放tts消息
-            val tts = this.tts
-            if (tts == null) {
-                ttsPrepare = TextToSpeech.ERROR
-                this.tts = TextToSpeech(context) {
-                    ttsPrepare = it
+        when (msg.what) {
+            WhatTimeNow -> {
+                timerHandler.removeMessages(1)//清除语音消息
+                timerHandler.removeMessages(2)//清除释放tts消息
+                val tts = this.tts
+                if (tts == null) {
+                    ttsPrepare = TextToSpeech.ERROR
+                    this.tts = TextToSpeech(context) {
+                        ttsPrepare = it
+                        timerHandler.sendEmptyMessageDelayed(1, 1000)
+                    }
+                } else if (TextToSpeech.SUCCESS != ttsPrepare) {
                     timerHandler.sendEmptyMessageDelayed(1, 1000)
-                }
-            } else if (TextToSpeech.SUCCESS != ttsPrepare) {
-                timerHandler.sendEmptyMessageDelayed(1, 1000)
-            } else {
-                calendar.timeInMillis = System.currentTimeMillis()
-                val hour = calendar[Calendar.HOUR_OF_DAY]
-                val minute = calendar[Calendar.MINUTE]
-                val second = calendar[Calendar.SECOND]
-                val nextDelay = (
-                        (if (minute <= 1) {
-                            speech(tts, "现在时间,${hour}点整")
-                            30
-                        } else if (minute >= 59) {
-                            speech(tts, "现在时间,${hour + 1}点整")
-                            30
-                        } else {
-                            speech(tts, "现在时间,${hour}点${minute}分")
-                            if (minute < 30) {
-                                30 - minute
-                            } else {
-                                60 - minute
-                            }
-                        }) * 60 - second) * 1000
-                /*
-                val nextDelay = (if (second < 19) {
-                            20 - second
-                        } else if (second < 39) {
-                            40 - second
-                        } else {
-                            60 - second
-                        }) * 1000
-                        */
+                } else {
+                    val calendar = Calendar.getInstance()
+                    calendar.timeInMillis = System.currentTimeMillis()
+                    val hour = calendar[Calendar.HOUR_OF_DAY]
+                    val minute = calendar[Calendar.MINUTE]
+                    val second = calendar[Calendar.SECOND]
 
-                //释放tts
-                timerHandler.sendEmptyMessageDelayed(2, 10 * 1000)
-                //下次播报时间
-                calendar.timeInMillis = calendar.timeInMillis + nextDelay
-                val nextReportMsg = "下次播报时间：${dateFormat.format(calendar.time)}"
-                Log.i("TimeNow", nextReportMsg)
-                liveMsg.postValue(nextReportMsg)
-                timerHandler.sendEmptyMessageDelayed(1, nextDelay.toLong())
+                    val nextDelay = ((if (minute <= 1) {
+                        speech(tts, "现在时间,${hour}点整")
+                        30
+                    } else if (minute >= 59) {
+                        speech(tts, "现在时间,${hour + 1}点整")
+                        30
+                    } else {
+                        speech(tts, "现在时间,${hour}点${minute}分")
+                        if (minute < 30) {
+                            30 - minute
+                        } else {
+                            60 - minute
+                        }
+                    }) * 60 - second) * 1000
+
+
+                    // = (2 * 60 - second) * 1000
+
+                    //释放tts
+                    timerHandler.sendEmptyMessageDelayed(2, 10 * 1000)
+                    //下次播报时间
+                    calendar.timeInMillis = calendar.timeInMillis + nextDelay
+                    val nextReportMsg = "下次播报时间：${dateFormat.format(calendar.time)}"
+                    val nextAlert = calendar.timeInMillis
+                    if (nextAlert != this.nextAlert) {
+                        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                        val intent = Intent(context, TimeNowService::class.java)
+
+                        AlarmManagerCompat.setAndAllowWhileIdle(alarmManager, AlarmManager.RTC_WAKEUP, nextAlert, PendingIntent.getService(context, 1, intent, PendingIntent.FLAG_ONE_SHOT))
+                    }
+                    Log.i("TimeNow", nextReportMsg)
+                    liveMsg.postValue(nextReportMsg)
+                }
+            }
+            WhatReleaseTts -> {
+                val tts = this.tts
+                if (tts != null) {
+                    tts.shutdown()
+                    this.tts = null
+                    ttsPrepare = TextToSpeech.ERROR
+                    Log.i("NowTime", "释放TTS")
+                }
             }
         }
         return true
+    }
+
+    fun timeNow() {
+        timerHandler.sendEmptyMessage(WhatTimeNow)
     }
 
     private fun speech(tts: TextToSpeech, s: String) {
@@ -105,11 +116,18 @@ class TimeNowWorker(val context: Context) : Handler.Callback {
     }
 
     fun onDestroy() {
+        //释放tts
         val tts = this.tts
         if (tts != null) {
             tts.shutdown()
             this.tts = null
         }
+        //取消闹钟
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, TimeNowService::class.java)
+        val pi = PendingIntent.getService(context, 1, intent, PendingIntent.FLAG_ONE_SHOT)
+        alarmManager.cancel(pi)
+        //移除消息并关闭
         timerHandler.removeCallbacksAndMessages(null)
         try {
             if (SDK_INT >= 18) {
@@ -120,5 +138,6 @@ class TimeNowWorker(val context: Context) : Handler.Callback {
         } catch (e: Exception) {
             e.printStackTrace()
         }
+
     }
 }
